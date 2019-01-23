d221e5b03cfcd237ce38cf2d46428aee11f26cff
Steven Olufemi-OluKa <Steven.Olufemi-OluKanni@jet2.com>
2019-01-23 11:35:43 +0000

    managed to get one box to flash

--- js/main.js
+++ js/main.js
@@ -1,31 +1,56 @@
 var app = new Vue({
   el: '#game',
+  data: {
+    randomColor: 0,
+    counter: 0,
+    randomBox: 0,
+    computersChoice : [],
+    boxes: 0,
+    level: 1,
+    colourBoxInterval: 0,
+    promptToEnter: "Copy the pattern"
+
+  },
   methods: {
     makeColoursFlashSetInterval(){
-      setInterval(this.makeColoursFlash,500);
+      this.colourBoxInterval =  setInterval(this.makeColoursFlash,500);
     },
 
     makeColoursFlash(){
       // if a box is shaded a colour then change it to white
-      if(randomColor != 0){
-        randomBox.style.backgroundColor = 'white';
-        randomColor = 0;
-        counter++;
-        checkCounter();
+      if(this.randomColor != 0){
+        this.randomBox.style.backgroundColor = 'white';
+        this.randomColor = 0;
+        this.counter++;
+        this.checkCounter
 
       } else {
 
         //selects a random box, fills it with a colour
-        randomBox = boxes[Math.floor(Math.random() * 4)];
-        randomColor = randomBox.getAttribute('id');
-        computersChoice.push(randomColor);
-        console.log(computersChoice);
-        randomBox.style.backgroundColor = randomColor;
+        this.boxes = document.getElementsByClassName('boxes');
+        this.randomBox = this.boxes[Math.floor(Math.random() * 4)];
+        this.randomColor = this.randomBox.getAttribute('id');
+        this.computersChoice.push(this.randomColor);
+        console.log(this.computersChoice);
+        this.randomBox.style.backgroundColor = this.randomColor;
 
       }
-      
+
+    },
+
+
+  },
+
+  computed: {
+    checkCounter(){
+      if(this.counter == this.level){
+        clearInterval(this.colourBoxInterval);
+        this.counter = 0;
+        return true
+      }
     }
 
-
   }
+
+
 })
